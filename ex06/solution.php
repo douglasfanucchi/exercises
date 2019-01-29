8a9f9b90c0e940dccf67a1287e60b4c0c16f3521
Douglas Fanucchi <douglasfanucchi@gmail.com>
2019-01-29 09:58:02 -0200

    problem 6 solved

---
+++ ex06/solution.php
@@ -0,0 +1,19 @@
+<?php
+	function birthdayCakeCandles($ar) {
+
+		$biggest = 0;
+		$times = 1;
+
+		foreach( $ar as $value ) {
+			if($value > $biggest) { 
+				$biggest = $value;
+				$times = 1;
+			}else if( $value === $biggest) {
+				$times++;
+			}
+		}
+
+		return $times;
+
+	}
+	
